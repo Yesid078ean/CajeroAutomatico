5bf366e3083e70210efd5275cd31d3cc82a9b486
Yesid078ean <yalarcon78@universidad.edu.co>
2017-09-11 21:45:04 -0500

    nuestro trabajo colaborativo Cajero automatico

---
+++ JavaApplication1.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication1;
+
+import java.sql.ResultSet;
+import java.util.List;
+
+
+
+public class JavaApplication1 {
+       
+   
+    
+    
+    public static void main(String[] args) {
+        
+     Ingreso VentanaIngreso = new Ingreso();
+     Opciones VentanaOpciones = new Opciones();
+     
+     
+     
+     VentanaIngreso.setVisible(true);
+        
+        
+    
+    
+    
+    
+    
+    }
+}
+
